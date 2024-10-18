d17d3845eb96b36440190077207ec7a2bfdff2dd
Michael <muxaujish@gmail.com>
2024-10-19 02:34:34 +0300

    Настроил композ для запуска базы и миграции через ликвибейз, переделал файлы миграции с yaml на sql

---
+++ src/main/resources/import_data/6_verter.sql
@@ -0,0 +1,7 @@
+INSERT INTO verter ("check", state, time) VALUES
+(3, 'Start', '18:36'),
+(3, 'Failure', '18:37'),
+(7, 'Start', '10:15'),
+(7, 'Success', '10:16'),
+(8, 'Start', '10:41'),
+(8, 'Success', '10:41');
---
+++ src/main/resources/import_data/7_transferred_points.sql
@@ -0,0 +1,8 @@
+INSERT INTO transferred_points (checking_peer, checked_peer, points_amount)
+VALUES
+    ('Baboba', 'Aboba', 1),
+    ('Cagoga', 'Baboba', 1),
+    ('Cagoga', 'Aboba', 1),
+    ('Egogo', 'Derevo', 1),
+    ('Derevo', 'Cagoga', 1),
+    ('Derevo', 'Egogo', 1);
