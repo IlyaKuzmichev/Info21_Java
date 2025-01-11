b0615e87f5bab007885e1cae295e8d952b2c4399
Michael <muxaujish@gmail.com>
2025-01-11 18:17:04 +0300

    Главная страница и отдельно шаблон с меню

--- src/main/java/edu/school21/info21/controller/MainController.java
+++ src/main/java/edu/school21/info21/controller/MainController.java
@@ -1,15 +1,29 @@
 package edu.school21.info21.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    // Обрабатываем запрос к главной странице
     @GetMapping("/")
-    public String index() {
-        // Возвращаем имя шаблона главной страницы
+    public String homePage() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/data")
+    public String dataPage() {
+        return "data";
+    }
+
+    @GetMapping("/operations")
+    public String operationsPage(Model model) {
+        return "operations";
     }
 }
