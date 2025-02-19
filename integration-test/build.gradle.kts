1e0b0905040a2d1c98fe4b2c9b5a84ef945b0b16
Michael <muxaujish@gmail.com>
2025-02-19 23:31:22 +0300

    small fixes

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -8,30 +8,19 @@ plugins {
 
 repositories {
     mavenCentral()
-    google()
-    gradlePluginPortal()
 }
 
 dependencies {
-    implementation(project(":")) // Подключаем код основного приложения
-
-    // Для отправки REST-запросов
+    // Подключаем код основного приложения
+    implementation(project(":"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    // Для работы с JPA (если нужно напрямую работать с базой)
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // Тестовые зависимости
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    // Для работы с REST-запросами в тестах
     testImplementation("io.rest-assured:rest-assured:5.4.0")
-
-    implementation("org.postgresql:postgresql:42.7.2")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform() // Включает JUnit 5
+    useJUnitPlatform()
 }
 
 tasks.named<BootJar>("bootJar") {
--- integration-test/src/test/java/rest/PeersTest.java
+++ integration-test/src/test/java/rest/PeersTest.java
@@ -5,7 +5,6 @@ import edu.school21.info21.dto.PeersDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -16,18 +15,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestConfig.class)
 public class PeersTest {
-    @LocalServerPort
-    private int port;  // Динамический порт
 
     @Autowired
     private RestTemplate restTemplate;
 
     @Test
     void testGetPeers() {
-        // Формируем URL с динамическим портом
-        String url = "http://localhost:" + port + "/api/v1/peers";
+        String url = "http://localhost:8080/api/v1/peers";
 
-        // Выполняем GET запрос к контроллеру
         ResponseEntity<List<PeersDTO>> responseEntity = restTemplate.exchange(
                 url,
                 org.springframework.http.HttpMethod.GET,
@@ -36,10 +31,8 @@ public class PeersTest {
                 }
         );
 
-        // Проверка статуса ответа
         assertEquals(200, responseEntity.getStatusCode().value());
 
-        // Проверка содержимого ответа
         var peers = responseEntity.getBody();
         assert peers != null && !peers.isEmpty();
         assertEquals("elevante", peers.getFirst().getNickname());
