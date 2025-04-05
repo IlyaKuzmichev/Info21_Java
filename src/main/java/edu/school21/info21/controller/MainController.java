12d2d6f2338a872ca64f5cbb38729ca0e93dd53f
Michael <muxaujish@gmail.com>
2025-04-05 14:49:39 +0300

    Добавил логирование

--- src/main/java/edu/school21/info21/controller/MainController.java
+++ src/main/java/edu/school21/info21/controller/MainController.java
@@ -1,11 +1,13 @@
 package edu.school21.info21.controller;
 
 import edu.school21.info21.services.QueryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class MainController {
 
@@ -17,21 +19,25 @@ public class MainController {
 
     @GetMapping("/")
     public String homePage() {
+        log.info("Пользователь зашел на главную страницу");
         return "index";
     }
 
     @GetMapping("/about")
     public String aboutPage() {
+        log.info("Пользователь зашел на страницу о проекте");
         return "about";
     }
 
     @GetMapping("/data")
     public String dataPage() {
+        log.info("Пользователь зашел на страницу с данными");
         return "data";
     }
 
     @GetMapping("/operations")
     public String operationsPage(Model model) {
+        log.info("Пользователь зашел на страницу с операциями");
         model.addAttribute("queries", queryService.getAllQueries());
         return "operations";
     }
--- src/main/java/edu/school21/info21/controller/TasksController.java
+++ src/main/java/edu/school21/info21/controller/TasksController.java
@@ -4,6 +4,7 @@ import edu.school21.info21.dto.TasksDTO;
 import edu.school21.info21.mappers.TasksMapper;
 import edu.school21.info21.model.Tasks;
 import edu.school21.info21.services.TasksService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequestMapping("/tasks")
 public class TasksController {
@@ -27,6 +29,7 @@ public class TasksController {
 
     @GetMapping
     public String tasks(Model model) {
+        log.info("Пользователь зашел на страницу с задачами");
         List<TasksDTO> tasksList = tasksService.getAllTasks(); // Получаем список задач
         model.addAttribute("tasks", tasksList);
         model.addAttribute("activePage", "tasks");
@@ -35,6 +38,7 @@ public class TasksController {
 
     @GetMapping("/{title}")
     public String getTaskByTitle(@PathVariable String title, Model model) {
+        log.info("Пользователь зашел на страницу с задачей {}", title);
         Optional<Tasks> task = tasksService.getTaskByTitle(title);
         if (task.isPresent()) {
             TasksDTO taskDTO = tasksMapper.toDTO(task.get());
@@ -42,6 +46,7 @@ public class TasksController {
             model.addAttribute("activePage", "tasks");
             return "task";
         } else {
+            log.error("Задание с названием {} не найдено", title);
             model.addAttribute("error", "Task with title " + title + " not found");
             return "error";
         }
@@ -49,6 +54,7 @@ public class TasksController {
 
     @GetMapping("/add")
     public String showAddForm(Model model) {
+        log.info("Пользователь зашел на страницу добавления задачи");
         model.addAttribute("task", new TasksDTO());
         model.addAttribute("projects", tasksService.getAllProjects());
         model.addAttribute("activePage", "tasks");
@@ -57,6 +63,7 @@ public class TasksController {
 
     @GetMapping("/edit/{title}")
     public String showEditForm(@PathVariable String title, Model model) {
+        log.info("Пользователь зашел на страницу редактирования задачи {}", title);
         Optional<Tasks> task = tasksService.getTaskByTitle(title);
         if (task.isPresent()) {
             model.addAttribute("task", tasksMapper.toDTO(task.get()));
@@ -64,6 +71,7 @@ public class TasksController {
             model.addAttribute("activePage", "tasks");
             return "task-form";
         } else {
+            log.error("Задание с названием {} не найдено", title);
             model.addAttribute("error", "Task with title " + title + " not found");
             return "error";
         }
@@ -71,12 +79,14 @@ public class TasksController {
 
     @PostMapping
     public String saveTask(@RequestParam String oldTitle, @ModelAttribute TasksDTO task) {
+        log.info("Пользователь сохраняет задачу {}", task.getTitle());
         tasksService.saveOrUpdateTask(oldTitle, task);
         return "redirect:/tasks";
     }
 
     @GetMapping("/remove/{title}")
     public String deleteTask(@PathVariable String title) {
+        log.warn("Пользователь удаляет задачу {}", title);
         tasksService.deleteTask(title);
         return "redirect:/tasks";
     }
